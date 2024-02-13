b1255c711fc3e10b6f3243268419bfa3e7e67c99
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2024-02-13 04:07:29 -0600

    Use python setup.py install

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,5 +1,5 @@
 @rem Remember to source the compiler
 
 set MKLROOT=%CONDA_PREFIX%
-%PYTHON% setup.py install --old-and-unmanageable
+%PYTHON% setup.py install
 if errorlevel 1 exit 1
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,4 +2,4 @@
 
 export CFLAGS="-I$PREFIX/include $CFLAGS"
 export MKLROOT=$CONDA_PREFIX
-$PYTHON -m pip install . --no-build-isolation --no-deps
+$PYTHON setup.py install
