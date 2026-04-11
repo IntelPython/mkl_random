42f9b707edccbf4090a0aed4fb3b9130cb977ff9
Nikita Grigorian <nikita.grigorian@intel.com>
2026-04-10 18:47:58 -0700

    fix indentation in get_state

--- mkl_random/interfaces/_numpy_random.py
+++ mkl_random/interfaces/_numpy_random.py
@@ -95,12 +95,13 @@ class RandomState(
 
             If `legacy` is False, a dictionary containing the state information
             is returned instead, with the following keys:
-                1. `bit_generator`: a string specifying the basic psedo-random
-                    number generation algorithm. It should always be `MT19937`
-                    for this class.
-                2. `state`: a dictionary guaranteed to contain the key
-                `mkl_stream`, whose value is a bytes object holding content of
-                Intel MKL's stream for the generator.
+
+            1. `bit_generator`: a string specifying the basic psedo-random
+                number generation algorithm. It should always be `MT19937`
+                for this class.
+            2. `state`: a dictionary guaranteed to contain the key
+            `mkl_stream`, whose value is a bytes object holding content of
+            Intel MKL's stream for the generator.
 
             Compare with `numpy.random.get_state`.
 
