ee60aac0b8c02b8a8036497485faf4c7133441b4
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2026-08-11 08:13:36 -0700

    Bump the version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "mkl_random"
-copyright = "2017-2025, Intel Corp."
+copyright = "2017-2026, Intel Corp."
 author = "Intel Corp."
-release = "1.5.0dev0"
+release = "1.5.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
